0307542b81c4d84e1db2cb5c5d2752a37953607d
attuo <attuo@protonmail.com>
2020-01-18 20:25:33 +0200

    Add basic structure for the project

---
+++ Web/Controllers/Api/BaseApiController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : Controller
+    { }
+}
