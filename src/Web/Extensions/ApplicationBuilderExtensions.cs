763ba0863e1a313f1263deba03e4964ecb9ee74e
attuo <attuo@protonmail.com>
2020-05-22 09:19:49 +0000

    Refactor ApplicationBuilder to be configured with extension methods

---
+++ src/Web/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWebApiTemplate.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+
+        /// <summary>
+        /// Development tools and other development related settings
+        /// Should be run first
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder ConfigureDevelopmentSettings(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            return app;
+        }
+
+        /// <summary>
+        /// Swagger configurations
+        /// Should be run before routing configurations
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder ConfigureSwagger(this IApplicationBuilder app)
+        {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Template API V1");
+            });
+
+            return app;
+        }
+
+        /// <summary>
+        /// Routing related configurations
+        /// This should be run last in Startup.cs configure
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder ConfigureRouting(this IApplicationBuilder app)
+        {
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            return app;
+        }
+
+    }
+}
--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using AspNetCoreWebApiTemplate.Extensions;
 
 using Web.Extensions;
 
@@ -28,38 +29,17 @@ namespace Web
         {
             services
                 .ConfigureSwagger()
-                .AddApplicationServices();
-            
-            services.AddControllers();
+                .AddApplicationServices()
+                .AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
-
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            // specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-            });
-
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app
+                .ConfigureDevelopmentSettings(env)
+                .ConfigureSwagger()
+                .ConfigureRouting();
         }
     }
 }
