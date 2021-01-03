26d5e881707f7e1eb764589a0d0d72f147bba384
Attuo <attuo@protonmail.com>
2021-01-03 17:56:43 +0200

    Add more code structure for unit tests

---
+++ tests/UnitTests/Core/Services/TodoService_Tests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebAPITemplate.UnitTests.Core.Services
+{
+    public class TodoService_Tests
+    {
+
+        #region Get All
+
+        public void GetTodos_Is_Ok()
+        {
+
+        }
+
+        #endregion
+
+        #region Get
+
+        public void GetTodo_Is_Ok()
+        {
+
+        }
+
+        #endregion
+
+        #region Create
+
+        public void CreateTodo_Is_Ok()
+        {
+
+        }
+
+        #endregion
+
+        #region Update
+
+        public void UpdateTodo_Is_Ok()
+        {
+
+        }
+
+        #endregion
+
+        #region Delete
+
+        public void DeleteTodo_Is_Ok()
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ tests/UnitTests/Web/Controllers/Api/TodosController_Tests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyWebAPITemplate.UnitTests.Web.Controllers.Api
+{
+    public class TodosController_Tests
+    {
+        #region Get all
+
+        [Fact]
+        public void Get_All_Is_Ok()
+        {
+
+        }
+
+        #endregion
+
+        #region Get
+
+        public void Get_Is_Ok()
+        {
+
+        }
+
+        #endregion
+
+        #region Create
+
+        public void Post_Is_Ok()
+        {
+
+        }
+
+        #endregion
+
+        #region Update
+
+        public void Put_Is_Ok()
+        {
+
+        }
+
+        #endregion
+
+        #region Delete
+
+        public void Delete_Is_Ok()
+        {
+
+        }
+
+        #endregion
+    }
+}
